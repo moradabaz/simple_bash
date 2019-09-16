e9e55da85b783b3079e10c5ddf778063d3e3d8df
morad <moradabaz@gmail.com>
2019-09-16 14:14:39 +0200

    Primer commit

---
+++ Desktop/ASO/simplesh/cmake-build-debug/CMakeFiles/simplesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplesh.dir/simplesh.c.o"
+  "simplesh.pdb"
+  "simplesh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simplesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
